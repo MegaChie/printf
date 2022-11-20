914e653960e0515cbb4898fd5c04bf15ebf8cdc3
MegaChie <workspace.mega@gmail.com>
2022-11-20 20:06:22 +0200

    update

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 int _printf(const char *format, ...);
--- print.c
+++ print.c
@@ -5,5 +5,16 @@
  */
 int _printf(const char *format, ...);
 {
-	int lettersCount;
+	int lettersCount, i;
+	char toPrint;
+
+	if (format == 0)
+	{
+		return (-1);
+	}
+	va_start(list, format)
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		
+	}
 }
